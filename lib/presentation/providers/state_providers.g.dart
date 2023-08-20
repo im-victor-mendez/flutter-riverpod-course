cbb8fb660b4f13ccba088c63e95c56f8a56fbab7
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-08-20 09:48:18 -0600

    feat: Implemented counter state_provider

---
+++ lib/presentation/providers/state_providers.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$counterHash() => r'a021a5ff7b694343160c76ffe5adfbf028676c3a';
+
+/// See also [Counter].
+@ProviderFor(Counter)
+final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
+  Counter.new,
+  name: r'counterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$counterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Counter = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
