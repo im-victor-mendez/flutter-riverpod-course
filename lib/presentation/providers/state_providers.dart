c1ad6471fe34e555089dbabeee8d35cca751b6dc
Victor Mendez <victor.mmc.pl@hotmail.com>
2023-08-21 09:12:07 -0600

    ci: Defined dark mode state provider

--- lib/presentation/providers/state_providers.dart
+++ lib/presentation/providers/state_providers.dart
@@ -11,3 +11,13 @@ class Counter extends _$Counter {
     state++;
   }
 }
+
+@riverpod
+class DarkMode extends _$DarkMode {
+  @override
+  bool build() => false;
+
+  void toggleDarkMode() {
+    state = !state;
+  }
+}
